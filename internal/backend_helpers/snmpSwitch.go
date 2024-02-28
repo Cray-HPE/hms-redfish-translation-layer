d522461d630d589dd931194334f94ce723079668
Joshua Williams <joshuaw@hpe.com>
2024-02-28 15:18:15 -0800

    Revert "CASMTRIAGE-6698: Reduce size of critical section protected by mutex"
    This reverts commit bd8ce10d180107e9c9cfd295f59545860abb2631.
    
    Reducing critical section was leaves pipeline dependencies unprotected.

--- internal/backend_helpers/snmpSwitch.go
+++ internal/backend_helpers/snmpSwitch.go
@@ -208,7 +208,13 @@ func (helper *SNMPSwitchHelper) initDevice(ctx context.Context, xname string, de
 	helper.RedisHelper.addMemberToSet(rootKeyspace, "Chassis")
 	helper.RedisHelper.addMemberToSet(rootKeyspace, "Managers")
 
-	// Call init functions
+	// Create a new pipeline
+
+	// The Redis pipeline is shared between Go threads so we need to protect this section of code with a mutex
+	helper.redisActivePipelineMux.Lock()
+
+	helper.RedisHelper.RedisActivePipeline = helper.RedisHelper.Redis.Pipeline()
+
 	initFunctions := [...]func(string, snmp_utilities.EntityPhysicalTable) error{
 		helper.initChassis,
 		helper.initManagers,
@@ -222,16 +228,14 @@ func (helper *SNMPSwitchHelper) initDevice(ctx context.Context, xname string, de
 			logFields["err"] = err
 			log.WithFields(logFields).Error("Initialization function failed")
 
+			helper.redisActivePipelineMux.Unlock()
+
 			return
 		} else {
 			log.WithFields(logFields).Debug("Initialization function succeeded")
 		}
 	}
 	delete(logFields, "initFunction")
-
-	// Create a new pipeline
-	helper.redisActivePipelineMux.Lock()	// because pipeline shared amongst all Go routines
-	helper.RedisHelper.RedisActivePipeline = helper.RedisHelper.Redis.Pipeline()
 
 	// Dump this pipeline.
 	_, err = helper.RedisHelper.RedisActivePipeline.Exec()
